4ecb87a6e97af3ecc0d9243d9a83da383722e5c3
Zocktr0pey <151160859+Zocktr0pey@users.noreply.github.com>
2025-09-16 15:01:52 +0200

    CameraScript holt sich selbsständig das CameraHolder Gameobject + some cleaning

--- Assets/Scripts/Player/CameraScript.cs
+++ Assets/Scripts/Player/CameraScript.cs
@@ -2,18 +2,17 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    [SerializeField] private float sensitivity = 1f;
-    [SerializeField] private GameObject cameraHolder;
+    [SerializeField] 
+    private float sensitivity = 1f;
 
-    private CharacterController controller;
+    private GameObject cameraHolder;
     private InputManager inputManager;
-    private Transform cameraHolderTransform;
     private float maxAngle = 85f; // muss > 0 und < 90 sein
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        cameraHolder = transform.Find("CameraHolder")?.gameObject;
         inputManager = InputManager.Instance;
     }
 
