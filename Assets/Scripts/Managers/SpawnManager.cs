1b35d7ed82db5bd9c95f1fdc1b4db7fae094dbb2
xB3nnyx <waschkowskibenjamin@gmail.com>
2025-09-20 15:59:51 +0200

    Add WaveManager, SpawnManager - add EnemySpawnWave
    Add
    EnemySpawnWave
    - Amount Of Enemies
    - Enemy Common (+ more later for types)
    - Spawn Rate
    
    WaveManager
     - selects current Wave
     - sends current wave to SpawnManager
    
    SpawnManager
    - spawns wave (handles enemy amount, spawnrate)

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private static SpawnManager instance;
+    [SerializeField] private float spawnRadius = 75/2f;
+
+    // Erlaubt anderen Klassen auf den Manager zuzugreifen
+    public static SpawnManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    // Sorgt dafür das immer nur eine Instanz dieses Managers existiert
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Starte Coroutine zum Spawnen der Welle
+    public void SpawnWave(EnemySpawnWave wave)
+    {
+        StartCoroutine(SpawnWaveCoroutine(wave));
+    }
+
+    //Schleife zum spawnen aller Gegner einer Welle
+    private IEnumerator SpawnWaveCoroutine(EnemySpawnWave wave)
+    {
+        for (int i = 0; i < wave.amountOfEnemies; i++)
+        {
+            SpawnEnemy(wave.enemyCommon);
+            yield return new WaitForSeconds(wave.spawnRate);
+        }
+
+        ////!ONLY FOR DEBUG
+        for (int i = 0; i < wave.amountOfEnemies; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            WaveManager.Instance.UpdateEnemiesCountMinus();
+        }
+    }
+
+    //Spawnt einzelne Gegner
+    public void SpawnEnemy(GameObject enemyObject)
+    {
+        // Errechne Spawnlocation
+        Vector3 spawnLocation = RandomPointByRadius(spawnRadius);
+
+        // gedreht zum Punkt (0, 0, 0)
+        Quaternion rotation = Quaternion.Euler(0, 0, 0);
+
+        //Spawne Gegner
+        Instantiate(enemyObject, spawnLocation, rotation);
+
+        WaveManager.Instance.UpdateEnemiesCountPlus();
+
+        return;
+    }
+
+    //Errechnet zufällige Koordinaten anahnd des Radius
+    private static Vector3 RandomPointByRadius(float radius)
+    {
+        //Höhe
+        float y = 0.5f;
+
+        // Zufallswinkel zwischen 0 und 2*Pi
+        float alpha = Random.value * 2f * Mathf.PI;
+
+        // Punkte auf dem Kreisrand
+        float x = (float)(radius * Mathf.Cos(alpha));
+        float z = (float)(radius * Mathf.Sin(alpha));
+
+        return new Vector3(x, y, z);
+    }
+}
