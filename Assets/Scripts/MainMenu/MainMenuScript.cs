c6dc9e3e12a78b567095bdb8950713c63a4c3cbe
xB3nnyx <waschkowskibenjamin@gmail.com>
2025-10-29 16:08:17 +0100

    add main menu with buttons

---
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartButton()
+    {
+        // Startet Spiel-Szene
+        SceneManager.LoadScene("Game");
+    }
+
+    void QuitButton()
+    {
+    #if UNITY_EDITOR
+            // Quit funktioniert nicht im Editor, stattdessen Playmode falsch setzen
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit();
+    #endif
+    }
+}
